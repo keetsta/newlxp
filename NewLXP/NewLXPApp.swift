317ccb75a444c13ff98a6a631183b4b366e5e8c5
keet <keetsta@icloud.com>
2025-11-25 11:39:33 +0300

    Initial commit

---
+++ NewLXP/NewLXPApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewLXPApp.swift
+//  NewLXP
+//
+//  Created by keet on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewLXPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
